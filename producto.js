b7f8f46cb305c51ba7999cb8fe4190b1861423b7
iUnknownWs <109998892+iUnknownWs@users.noreply.github.com>
2024-01-03 16:51:51 -0400

    Initial Commit

---
+++ producto.js
@@ -0,0 +1,64 @@
+let gallery = document.getElementById('gallery');
+let modal = document.getElementById('gallery-full');
+let fnext = document.getElementById('fnext');
+let fprev = document.getElementById('fprev');
+
+gallery.addEventListener('click', function () {
+    let div = document.querySelector('.active');
+    let image = div.querySelector('img');
+    let imageUrl = image.src;
+    let imgf = document.getElementById('img-full');
+    imgf.src = imageUrl;
+
+    console.log(imageUrl);
+    modal.style.display = 'block';
+});
+
+fnext.addEventListener('click', function () {
+    let div = document.getElementById('gallery');
+    let childs = div.children;
+    let imgf = document.getElementById('img-full');
+
+    for (let i = 0; i < childs.length; i++) {
+        if (childs[i].classList.contains('active')) {
+            if (childs[i + 1] == undefined) {
+                return
+            }
+            let next = childs[i + 1];
+            console.log(next);
+            next.classList.add('active');
+            childs[i].classList.remove('active');
+            let image = next.querySelector('img');
+            let imageUrl = image.src;
+            imgf.src = imageUrl;
+            return
+        }
+    }
+})
+
+fprev.addEventListener('click', function () {
+    let div = document.getElementById('gallery');
+    let childs = div.children;
+    let imgf = document.getElementById('img-full');
+
+    for (let i = 0; i < childs.length; i++) {
+        if (childs[i].classList.contains('active')) {
+            if (childs[i - 1] == undefined) {
+                return
+            }
+            let next = childs[i - 1];
+            console.log(next);
+            next.classList.add('active');
+            childs[i].classList.remove('active');
+            let image = next.querySelector('img');
+            let imageUrl = image.src;
+            imgf.src = imageUrl;
+            return
+        }
+    }
+})
+
+let closeButton = modal.getElementsByClassName('close')[0];
+closeButton.addEventListener('click', function () {
+    modal.style.display = 'none';
+});
